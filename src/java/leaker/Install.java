e318419c4a418fc0e7b13a66fedb1e42ff3a0347
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2013-02-04 07:57:43 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/leaker@341 302f0312-3c5d-0d4b-b714-136c201f073e

--- src/java/leaker/Install.java
+++ src/java/leaker/Install.java
@@ -6,9 +6,6 @@ package leaker;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,16 +15,12 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Quicksort
  */
-public class Setup extends HttpServlet {
+public class Install extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException, UnsupportedEncodingException {
-	try {
-	    User.Setup.CreateTable();
-	} catch (SQLException ex) {
-	    Logger.getLogger(Setup.class.getName()).log(Level.SEVERE, null, ex);
-	}
+	throws ServletException, IOException, UnsupportedEncodingException {
+            Application.Current().Install();
     }
 
 
